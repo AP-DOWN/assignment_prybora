010864212d7d492ee09e9a60090253d81bfffa2c
apd0wn <artempribora@gmail.com>
2025-09-08 11:37:18 +0300

    Code of the task 1

--- task1.py
+++ task1.py
@@ -1,2 +1,6 @@
-n = int(input('Paste ur number'))
-print (n+1)
+#9910
+a, b = map(int, input().split())
+left = min(a, b) + 1
+right = max(a, b) - 1
+count = (right + 1) // 2 - (left) // 2
+print(count)
